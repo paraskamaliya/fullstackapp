386877d5347a196e7124245b57dc0d96bdd6d07e
Paras <paraskamliya5@gmail.com>
2023-10-27 08:31:28 +0530

    done

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const { userRouter } = require("./routes/user.router");
 const { postRouter } = require("./routes/post.router");
 
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.get("/", (req, res) => {
